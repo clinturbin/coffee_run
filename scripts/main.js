4a2625797a4c81656ae2b8e1945fb7561c378017
clinturbin <clinturbin@gmail.com>
2018-07-31 11:54:45 -0400

    Refactored to make most functions global and most variables local.

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,102 @@
 var coffeeOrderForm = document.querySelector('[data-coffee-order="form"]');
 var pendingOrders = document.querySelector(".pending-orders");
-
-var ordersAsString = localStorage.getItem('coffee-orders');
-var ordersAsObject = JSON.parse(ordersAsString);
 var coffeeOrders = [];
 
-
-var loadOrdersFromStorage = function () {
-    if (ordersAsObject !== null) {
-        ordersAsObject.forEach( function (element) {
-            coffeeOrders.push(element);    
-        });
-    };
-};
-loadOrdersFromStorage();
-
 var addOrdersToLocalStorage = function () {
-    ordersAsString = JSON.stringify(coffeeOrders);
+    var ordersAsString = JSON.stringify(coffeeOrders);
     localStorage.setItem('coffee-orders', ordersAsString);
 };
 
-var clearOrderElements = function () {
+var getOrdersFromLocalStorage = function () {
+    var ordersAsString = localStorage.getItem('coffee-orders');
+    var ordersAsObjects = JSON.parse(ordersAsString);
+    if (ordersAsObjects !== null) {
+        ordersAsObjects.forEach(function (order) {
+            coffeeOrders.push(order);    
+        });
+    };
+};
+
+var clearPendingOrdersDisplay = function () {
     var orders = document.querySelectorAll('.order');
     orders.forEach( function (element) {
         element.remove();
-    })
+    });
 };
 
+var updatePendingOrders = function () {
+    clearPendingOrdersDisplay();
+    coffeeOrders.forEach(function (element, index) {
+        pendingOrders.appendChild(pendingOrder(element, index));
+    });
+    addOrdersToLocalStorage();
+};
 
+//---------------------  Conatiner for Each Pending Order --------------------------------
+var pendingOrder = function (order, index) {
+    var newOrder = document.createElement('div');
+    newOrder.classList.add('order');
+    newOrder.appendChild(orderSummary(order));
+    newOrder.appendChild(orderCompleteContainer(index));
+    return newOrder;
+};
+
+//------------ Order Summary Section includes information from input form ----------------
+var orderSummary = function (order) {
+    var orderSummary = document.createElement('div');
+    orderSummary.classList.add('order-summary');
+    orderSummary.appendChild(orderSummaryCoffee(order));
+    orderSummary.appendChild(orderSummaryDetails(order));
+    orderSummary.appendChild(orderSummaryEmail(order));
+    return orderSummary;
+}
+
+var orderSummaryCoffee = function (order) {
+    var coffeeOrdered = document.createElement('div');
+    coffeeOrdered.classList.add("order-summary-coffee");
+    coffeeOrdered.textContent = order.coffee;
+    return coffeeOrdered;
+};
+
+var orderSummaryDetails = function (order) {
+    var orderDetails = document.createElement("div");
+    orderDetails.classList.add("order-summary-details");
+    orderDetails.textContent = `${order.size}    |   ${order.flavor}   |   ${order.caffeine}`;
+    return orderDetails;
+};
+
+var orderSummaryEmail = function (order) {
+    var orderEmail = document.createElement("div");
+    orderEmail.classList.add("order-summary-email");
+    orderEmail.textContent = order.email;
+    return orderEmail;
+};
+
+//---------------------------------------------------------------------------
+var orderCompleteContainer = function (index) {
+    var inputContainer = document.createElement("div");
+    inputContainer.classList.add("order-complete");
+    var completeButton = newCompleteButton(index);
+    inputContainer.appendChild(completeButton);
+    return inputContainer;
+};
+
+var newCompleteButton = function (index) {
+    var completeButton = document.createElement("input");
+    completeButton.classList.add("complete-btn");
+    completeButton.setAttribute("type", "button");
+    completeButton.setAttribute("value", "Complete");
+    completeButton.addEventListener('click', function () {
+        coffeeOrders.splice(index, 1);
+        updatePendingOrders();
+    });
+    return completeButton;
+};
+
+// -------------------------------------------------------------------------------------
+
+getOrdersFromLocalStorage();
+updatePendingOrders();
 
 coffeeOrderForm.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -43,78 +112,5 @@ coffeeOrderForm.addEventListener('submit', function (event) {
                     'caffeine': caffeineRating
                    };
     coffeeOrders.push(newOrder);
-    addOrdersToLocalStorage();
     updatePendingOrders();
-    
 });
-
-var updatePendingOrders = function () {
-    clearOrderElements();
-    coffeeOrders.forEach(function (element, index) {
-        pendingOrders.appendChild(pendingOrder(element, index));
-    })
-    addOrdersToLocalStorage();
-};
-
-var pendingOrder = function (element, index) {
-    var newOrder = document.createElement('div');
-    newOrder.classList.add('order');
-    newOrder.appendChild(orderSummary(element));
-    newOrder.appendChild(orderComplete(index));
-    return newOrder;
-};
-
-var orderSummary = function (element) {
-    var coffeeOrdered = orderSummaryCoffee(element.coffee);
-    var orderDetails = orderSummaryDetails(element.size, element.flavor, element.caffeine);
-    var orderEmail = orderSummaryEmail(element.email);
-    var orderSummary = document.createElement('div');
-    orderSummary.classList.add('order-summary');
-    orderSummary.appendChild(coffeeOrdered);
-    orderSummary.appendChild(orderDetails);
-    orderSummary.appendChild(orderEmail);
-    return orderSummary;
-}
-
-var orderSummaryCoffee = function (coffeeType) {
-    var coffeeOrdered = document.createElement('div');
-    coffeeOrdered.classList.add("order-summary-coffee");
-    coffeeOrdered.textContent = coffeeType;
-    return coffeeOrdered;
-};
-
-var orderSummaryDetails = function (size, flavor, caffeine) {
-    var orderDetails = document.createElement("div");
-    orderDetails.classList.add("order-summary-details");
-    orderDetails.textContent = `${size}    --   ${flavor}   --   ${caffeine}`;
-    return orderDetails;
-};
-
-var orderSummaryEmail = function (email) {
-    var orderEmail = document.createElement("div");
-    orderEmail.classList.add("order-summary-email");
-    orderEmail.textContent = email;
-    return orderEmail;
-};
-
-var orderComplete = function (index) {
-    var inputContainer = document.createElement("div");
-    inputContainer.classList.add("order-complete");
-    var completeButton = makeCompleteButton(index);
-    inputContainer.appendChild(completeButton);
-    return inputContainer;
-};
-
-var makeCompleteButton = function (index) {
-    var completeBtn = document.createElement("input");
-    completeBtn.classList.add("complete-btn");
-    completeBtn.setAttribute("type", "button");
-    completeBtn.setAttribute("value", "complete");
-    completeBtn.addEventListener('click', function () {
-        coffeeOrders.splice(index, 1);
-        updatePendingOrders();
-    });
-    return completeBtn;
-};
-
-updatePendingOrders();
